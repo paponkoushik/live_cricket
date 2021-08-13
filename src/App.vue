98ce78c88c002dbb1cff3a904d08b7636ed563c5
paponkoushik <paponkoushik@gmail.com>
2021-08-13 08:59:16 +0600

    design complete

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Navigation />
+  </div>
+</template>
+
+<script>
+import Navigation from "./components/Navigation";
+export default {
+  name: 'App',
+  components: {
+    Navigation
+  }
+}
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import App from './App.vue';
+import {routes} from "./router/index";
+import VueRouter from "vue-router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://rest.entitysport.com/v2/";
+Vue.prototype.$accessToken = "437214169d9be2a73e91d22f76f68b52"
+
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+const router = new VueRouter({
+  routes,
+  mode:'history'
+});
+
+new Vue({
+  render: h => h(App),
+  router,
+}).$mount('#app')
